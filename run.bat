3b01e2b72285ceb66c18df16e09b1a229df156ca
MartiGames8899 <tiagosilvamartins95@gmail.com>
2026-03-01 22:05:34 +0000

    Backup automatico via run.bat

--- run.bat
+++ run.bat
@@ -5,13 +5,13 @@ REM Add custom program arguments {such as nogui} to this file in the next line b
 REM  pass them to this script directly
 
 echo Executando Git Pull...
-git pull origin main
+git pull origin zombies
 
 java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.20.1-47.4.16/win_args.txt %*
 
 echo Executando Git Push...
 git add .
 git commit -m "Backup automatico via run.bat"
-git push origin main
+git push origin zombies
 
 pause
